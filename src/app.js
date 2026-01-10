45eb98e8757b770d3f9b6c4f277aacd0792d9444
Vijay Kumar Yadav <stardust149489@gmail.com>
2026-01-10 09:30:37 +0530

    Implement CORS middleware for API
    Added CORS middleware to handle preflight requests and set headers.

--- src/app.js
+++ src/app.js
@@ -15,7 +15,7 @@ import userRoutes from "./routes/user.routes.js";
 import { errorHandler } from "./middlewares/error.middleware.js";
 
 const app = express();
-
+app.set("trust proxy", 1)
 /* ===============================
    GLOBAL MIDDLEWARES
 ================================ */
@@ -30,6 +30,18 @@ app.use(
     optionsSuccessStatus: 200
   })
 );
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+
+  next();
+});
+
 app.options("*", cors());
 
 // Body parsers
